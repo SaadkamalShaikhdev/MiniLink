ad372f745de2ecb2c3358df69ea9dc8b2d7c7853
SaadkamalShaikhdev <saadkamaldev@gmail.com>
2026-01-18 00:03:54 +0500

    change url from local to host

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -24,7 +24,7 @@ const Dashboard = () => {
   const getData = async () => {
     try {
       setLoading(true)
-      const fetchingData = await fetch("/api/dashboardData")
+      const fetchingData = await fetch(`${NEXT_PUBLIC_HOST}api/dashboardData`)
       
       if (!fetchingData.ok) {
         throw new Error(`HTTP error! status: ${fetchingData.status}`)
--- src/app/page.js
+++ src/app/page.js
@@ -24,7 +24,7 @@ const requestOptions = {
   redirect: "follow"
 };
 
-fetch("http://localhost:3000/api/generate", requestOptions)
+fetch(`${NEXT_PUBLIC_HOST}api/generate`, requestOptions)
   .then((response) => response.json())
   .then((result) =>{ alert(result.message)
  setgenerated(`${process.env.NEXT_PUBLIC_HOST}${shortUrl}`)
